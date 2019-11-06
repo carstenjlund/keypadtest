36e99b6ae9390b66c80d08c4bf860f9259d0674f
clurts <clu@rts.dk>
2019-11-06 21:12:56 +0100

    keypad initial

---
+++ app.js
@@ -0,0 +1,50 @@
+let thing = document.querySelector('.thing')
+let test = document.querySelector('#test')
+let circles = document.querySelectorAll('.circle')
+const numpad = document.querySelector('.numpad')
+console.log(thing);
+console.log(circles);
+
+var manualInputEvent = new Event('input', {
+    bubbles: true,
+    cancelable: true,
+});
+
+numpad.addEventListener('click', () => {
+    if (event.target !== event.currentTarget) {
+        console.log(event.target.value)
+        test.value += event.target.value
+        test.dispatchEvent(manualInputEvent);
+    }
+})
+
+test.addEventListener('input', () => {
+
+    circles.forEach((circle, index) => {
+        if (index < test.value.length) {
+            circle.classList.add('filled')
+        }
+    })
+
+    if (test.value.length === 4) {
+
+        test.setAttribute('disabled', '')
+        setTimeout(() => {
+            if (window.navigator.vibrate) {
+                window.navigator.vibrate(500);
+            }
+            document.querySelector('.holder').classList.add('shaker')
+            setTimeout(() => {
+                document.querySelector('.holder').classList.remove('shaker')
+                test.value = ''
+                test.removeAttribute('disabled')
+
+                circles.forEach((circle, index) => {
+                    circle.classList.remove('filled')
+                })
+            }, 750)
+        }, 450)
+    }
+
+
+})
